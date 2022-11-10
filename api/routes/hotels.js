675f092c39d6cec6cfee8174d3ea024776171364
Studawatta <st.udawatte@gmail.com>
2022-11-11 01:10:10 +0530

    new changes

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -17,6 +17,60 @@ const newHotel = new Hotel(req.body)
         res.status(500).json(err);
         
     }
-})
+});
+
+//Update
+
+router.put("/:id",async (req,res)=>{
+    try{
+        const updateHotel = await Hotel.findByIdAndUpdate(
+            req.params.id,
+            {$set:req.body},
+            {new:true}
+            );
+        res.status(200).json(updateHotel);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//Delete
+
+router.delete("/:id",async(req,res)=>{
+    try {
+        await Hotel.findByIdAndDelete(
+            req.params.id
+        );
+        res.status(200).json("Hotel has been deleted!");
+    } catch(err){
+        res.status(500).json(err);
+    }
+} );
+
+//Get one
+router.get("/:id", async(req,res)=>{
+    try{
+        const hotel = await Hotel.findById(
+            req.params.id,
+        );
+        res.status(200).json(hotel);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//Get All
+
+router.get("/",async(req,res,next)=>{
+
+  
+
+    try{
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+    }catch(err){
+        next(err);
+    }
+});
 
 export default router;
