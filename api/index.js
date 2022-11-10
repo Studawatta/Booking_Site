5f9eb8e86e1bc03504268e6358034e3b63eabc72
Studawatta <st.udawatte@gmail.com>
2022-11-10 22:00:26 +0530

    first

---
+++ api/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+const app = express();
+dotenv.config();
+
+const connect = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB.")
+    }catch(error){
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDB disconnected!");
+});
+mongoose.connection.on("connected",()=>{
+    console.log("mongoDB connected!");
+});
+
+//middlewares
+app.use(express.json());
+app.use("/api/auth",authRoute);
+app.use("/api/users",usersRoute);
+app.use("/api/hotels",hotelsRoute);
+app.use("/api/rooms",roomsRoute);
+
+app.listen(8080,()=>{
+    connect()
+    console.log("Connected to backend!");
+}) 
