ab44984948bf26921d7d979249769509948ec64f
Studawatta <st.udawatte@gmail.com>
2022-11-12 07:17:27 +0530

    // user authentication

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,19 +1,20 @@
 import express from "express";
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 //Create
-router.post("/",createHotel);
+router.post("/", verifyAdmin ,createHotel);
 
 //Update
 
-router.put("/:id",updateHotel);
+router.put("/:id", verifyAdmin ,updateHotel);
 
 //Delete
 
-router.delete("/:id",deleteHotel);
+router.delete("/:id", verifyAdmin , deleteHotel);
 
 //Get one
 router.get("/:id",getHotel);
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,26 +1,33 @@
 import express from "express";
 import { updateUser, deleteUser, getUser, getUsers } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser} from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req,res,next)=>{
-    res.send("hello user, you are logged in");
-})
+// router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+//     res.send("hello user, you are logged in");
+// });
+
+// router.get("/checkuser/:id",verifyUser  , (req,res,next)=>{
+//     res.send("hello user, you are logged in and you can delete your account");
+// });
+// router.get("/checkadmin/:id",verifyUser  , (req,res,next)=>{
+//     res.send("hello admin, you are logged in and you can delete all accounts");
+// });
 
 //Update
-
-router.put("/:id",updateUser);
+   
+router.put("/:id", verifyUser, updateUser);
 
 //Delete
 
-router.delete("/:id",deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 //Get one
-router.get("/:id",getUser);
+router.get("/:id", verifyUser, getUser);
 
 //Get All
 
-router.get("/",getUsers);
+router.get("/",verifyAdmin,getUsers);
 
 export default router;
