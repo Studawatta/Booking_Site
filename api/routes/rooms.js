47c608e1f670819c8fa876f33a16637f66cf6909
Studawatta <st.udawatte@gmail.com>
2022-11-12 08:25:05 +0530

    rooms functions

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,24 @@
 import express from "express";
-
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
+//Create
+router.post("/:hotelid", verifyAdmin ,createRoom);
 
+//Update
+
+router.put("/:id", verifyAdmin ,updateRoom);
+
+//Delete
+
+router.delete("/:id", verifyAdmin , deleteRoom);
+
+//Get one
+router.get("/:id",getRoom);
+
+//Get All
+
+router.get("/",getRooms);
 
 export default router;
