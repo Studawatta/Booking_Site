0e2e4c6a0a5bccd9456030b7a0d741755a3c013d
Studawatta <st.udawatte@gmail.com>
2022-11-14 20:27:30 +0530

    update

--- api/index.js
+++ api/index.js
@@ -48,7 +48,7 @@ app.use((err,req,res,next)=>{
     })
 });
 
-app.listen(8080,()=>{
+app.listen(8800,()=>{
     connect()
     console.log("Connected to backend!");
 }) 
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
@@ -17,10 +17,12 @@ router.put("/:id", verifyAdmin ,updateHotel);
 router.delete("/:id", verifyAdmin , deleteHotel);
 
 //Get one
-router.get("/:id",getHotel);
+router.get("/find/:id",getHotel);
 
 //Get All
 
 router.get("/",getHotels);
+router.get("/countByCity",countByCity);
+router.get("/countByType",countByType);
 
 export default router;
