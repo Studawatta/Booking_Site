8ee53d33450039651a971d415e09b37771d985c0
Studawatta <st.udawatte@gmail.com>
2023-01-21 20:23:44 +0530

    authenticaiton

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = {
   user: JSON.parse(localStorage.getItem("user")) || null,
   loading: false,
   error: null,
-};
+};   
 
 export const AuthContext = createContext(INITIAL_STATE);
 
